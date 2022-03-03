348e85f2db3523171c1f38deb0b26e8e2ac4dd02
Rodrigo667k <rodrigo-viana34@hotmail.com>
2022-03-03 15:34:06 -0300

    Implementação do get e  pot de tema

--- src/app/model/Tema.ts
+++ src/app/model/Tema.ts
@@ -1,6 +1,9 @@
 import { Postagem } from "./Postagem"
 
 export class Tema{
+    postTema(tema: Tema) {
+      throw new Error('Method not implemented.')
+    }
 
     public id: number
     public descricao: string
